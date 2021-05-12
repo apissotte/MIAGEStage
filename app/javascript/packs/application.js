0d7db322912b03b4025a85e3d5f708c140ffccdb
Alexandre PISSOTTE <orion.aurore@gmail.com>
2021-05-12 09:22:47 +0200

    Override Bootstrap 5

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "bootstrap/dist/js/bootstrap"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
