ba0797881f3de5028c1007c3030fd42fbda1357c
Alexandre PISSOTTE <orion.aurore@gmail.com>
2021-05-18 09:57:45 +0200

    Add not finished Etudiant Model

--- db/migrate/20210517171627_create_promotions.rb
+++ db/migrate/20210517171627_create_promotions.rb
@@ -3,5 +3,7 @@ class CreatePromotions < ActiveRecord::Migration[6.1]
     create_table :promotions do |t|
       t.string :annee, limit: 4
     end
+
+    add_index :promotions, :annee, unique: true
   end
 end
