c38ce336aa0f708076e9b74e922b619083e62a4d
Alexandre PISSOTTE <orion.aurore@gmail.com>
2021-05-21 09:17:09 +0200

    Styling

--- app/models/formation.rb
+++ app/models/formation.rb
@@ -1,7 +1,11 @@
 class Formation < ApplicationRecord
+  # Associations du modèle
   belongs_to :promotion
   has_and_belongs_to_many :etudiants
   has_many :disponibilites
 
+  # Enum
   enum mention: { L3: 'L3', M1: 'M1', M2: 'M2' }
+
+  validates :promotion_id, presence: true
 end
