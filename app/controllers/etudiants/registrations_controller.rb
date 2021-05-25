d5b6d6af928b130868ed5599f10f03376eda061e
Alexandre PISSOTTE <orion.aurore@gmail.com>
2021-05-25 18:35:42 +0200

    Patch Sign In Etudiants

--- app/controllers/etudiants/registrations_controller.rb
+++ app/controllers/etudiants/registrations_controller.rb
@@ -5,16 +5,33 @@ class Etudiants::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /etudiants/sign_up
-  def new
-    super
-    puts 'coucou'
-  end
+  # def new
+  #   super
+  # end
 
   # POST /etudiants
   def create
-    super
-    self.configure_sign_up_params
-    puts :attribute
+    build_resource(sign_up_params)
+
+    resource.email_universitaire = resource.email
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
   end
 
   # GET /etudiants/edit
