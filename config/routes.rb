9eed8f67d84a29d17be755f7184bfb030a78c8eb
Alexandre PISSOTTE <orion.aurore@gmail.com>
2021-05-25 14:39:31 +0200

    Add Depencency Devise and Configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # Require a root to for devise
+  root to: 'static#index'
+
   get '/static', to: 'static#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
