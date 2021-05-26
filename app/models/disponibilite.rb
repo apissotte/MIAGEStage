d9f882b6d83bb5194dac7bfd6cab9b3b888e2db5
Al. Grognet <palexis.grognet@gmail.com>
2021-05-26 23:17:31 +0200

    Harmonisation de la vue Disponibilite

--- app/models/disponibilite.rb
+++ app/models/disponibilite.rb
@@ -2,7 +2,9 @@ class Disponibilite < ApplicationRecord
   belongs_to :tuteur_universitaire
   belongs_to :promotion
 
-  validates :nb_etudiants_souhaite, numericality: {only_integer: true, greater_than_or_equal_to: 0}, presence: true
+  enum statut_reponse: { :OK => "OK", :ABANDON => "ABANDON", :PAS_DE_REPONSE => "PAS_DE_REPONSE" }
+
+  validates :nb_etudiants_souhaite, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, presence: true
   validates :tuteur_universitaire, uniqueness: { scope: :promotion }
 
 end
