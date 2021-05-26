9f9bef23c3389aa13925e38e8d861c2a312d820d
nncho <nchonaomie@gmail.com>
2021-05-26 12:03:15 +0200

    Guid inscription etudiant et tuteur universitaire

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,16 @@ Rails.application.routes.draw do
   devise_for :etudiants, controllers: {
     registrations: "etudiants/registrations"
   }
+  devise_scope :etudiant do
+    get '4d6a8d14-ef57-4da8-908d-5b3388bfdf48/sign_up', to: 'etudiants/registrations#new'
+  end
 
   devise_for :tuteur_universitaires, controllers: {
     registrations: "tuteur_universitaires/registrations"
   }
-
+  devise_scope :tuteur_universitaire do
+    get 'f5506526-0a7d-40af-a39f-e456e5991564/sign_up', to: 'tuteur_universitaires/registrations#new'
+  end
 
   # Require a root to for devise
   root to: 'static#index'
