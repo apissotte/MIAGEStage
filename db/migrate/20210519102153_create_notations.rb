839c10d539832d4eb361280150badae0b959e912
Alexandre PISSOTTE <orion.aurore@gmail.com>
2021-05-27 09:01:57 +0200

    Update Migration

--- db/migrate/20210519102153_create_notations.rb
+++ db/migrate/20210519102153_create_notations.rb
@@ -4,7 +4,7 @@ class CreateNotations < ActiveRecord::Migration[6.1]
       t.string :note
       t.string :commentaire
       t.bigint :stage_id
-      t.integer :notation_format_id
+      t.bigint :notation_format_id
     end
     add_foreign_key :notations, :stages
     add_foreign_key :notations, :notation_formats
