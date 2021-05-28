6b258da1b0a0421115942a7facc8feb56ed4a4a8
melaniefonseca <melanie93fonseca@gmail.com>
2021-05-28 14:10:59 +0200

    migration type text json evaluation contenu

--- db/migrate/20210527075115_change_columns_types.rb
+++ db/migrate/20210527075115_change_columns_types.rb
@@ -2,5 +2,6 @@ class ChangeColumnsTypes < ActiveRecord::Migration[6.1]
   def change
     change_column :ge_formats, :contenu, :text
     change_column :notation_formats, :contenu, :text
+    change_column :evaluations, :contenu, :text
   end
 end
